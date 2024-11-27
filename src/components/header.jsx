385bdbde95ceaf8f3da83b5356fd1445a660ad69
Lyn-Dah <edulind99@gmail.com>
2024-11-27 00:16:43 +0000

    add static header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/images/Agriquinns-logo1.jpg';
 const Header = () => {
   return (
     <header>
-      <nav class=" flex justify-between py-4 px-10 w-full left-0 top-0 absolute  bg-green-700 text-white">
+      <nav class="fixed top-0 left-0 w-full z-10 flex justify-between py-4 px-10 bg-green-700 text-white">
         <a href="#" class=" hover:text-black font-extrabold text-3xl font-sans">AGRIQUINNS</a>
         {/* <img src={logo} alt="Agriquinns Logo"  class="w-12 h-12 mr-4  "></img>
      */}

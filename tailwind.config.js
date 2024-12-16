f17138db870da4d889f34d573192c8a619b32aee
Lyn-Dah <edulind99@gmail.com>
2024-12-16 06:58:36 +0000

    css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
 module.exports = {
   mode: 'jit',
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
     },
+    screens: {
+      sm: '640px',
+      md: '1280px',
+      lg: '1281px',
+    },
   },
   variants: {},
   plugins: [],
-};
+}
+
